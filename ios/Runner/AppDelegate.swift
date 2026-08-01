058daa975e514de1f5a8d459ad470ea924e4de4c
seonwooj0810 <seonwooj0810@gmail.com>
2026-08-01 23:54:01 +0900

    chore(ios): iOS 플랫폼 설정 정비 및 Xcode/Flutter 버전 호환 갱신
    @UIApplicationMain을 @main으로 전환하고, Podfile/Podfile.lock 및
    Package.resolved를 최신 Flutter/Xcode 버전에 맞게 갱신. 최신 Flutter
    스킴 마이그레이션(customLLDBInitFile, PreActions 스크립트)을 반영하고
    release 빌드용 RunnerRelease.entitlements를 추가함.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import Flutter
 import UIKit
 import Firebase
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
